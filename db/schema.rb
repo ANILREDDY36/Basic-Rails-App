7faaeeb8d1c227cd71dfdbc2e7c1bfe9e1aaa73f
ANILREDDY36 <anilroyal867@gmail.com>
2019-10-15 17:01:10 +0530

    add language_id to words table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_110034) do
+ActiveRecord::Schema.define(version: 2019_10_15_112723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2019_10_15_110034) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "language_id"
+    t.index ["language_id"], name: "index_words_on_language_id"
   end
 
+  add_foreign_key "words", "languages"
 end
