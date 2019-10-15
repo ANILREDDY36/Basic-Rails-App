c5652d2abff385f19775517a991c1cbaab9181ac
ANILREDDY36 <anilroyal867@gmail.com>
2019-10-15 16:32:41 +0530

    remove language field from models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_104004) do
+ActiveRecord::Schema.define(version: 2019_10_15_110034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2019_10_15_104004) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "language"
   end
 
 end
