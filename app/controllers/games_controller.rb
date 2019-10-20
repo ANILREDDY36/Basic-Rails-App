ce4f2fa7320eda14083c8c3ece5ea85ab175ddbb
anil reddy <anilroyal867@gmail.com>
2019-10-20 22:39:29 +0530

    create and show game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# WordsController
+class GamesController < ApplicationController
+  # before_action :set_game, only: %i[show edit update destroy]
+  before_action :authenticate_user!, only: %i[show create]
+
+  def index
+    
+  end
+
+  def create
+    game = current_user.games.create
+    redirect_to(game)
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+end  
