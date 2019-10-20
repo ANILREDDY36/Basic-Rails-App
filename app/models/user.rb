5704f053a1bbf51dc511ad78431986aafcbdcd8b
anil reddy <anilroyal867@gmail.com>
2019-10-20 21:41:45 +0530

    hide email address and add specs to it

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :words       
+  has_many :words
+
+  def secret_email
+    email.gsub(/.{0,4}@/, '####@')
+  end    
 end
